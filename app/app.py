cb28ff6a52467506f599a9a1894a1bcc7d3213c5
Shaik Sahera <shaiksahera0786@gmail.com>
2026-08-10 13:41:47 +0530

    Fixed Streamlit import path

--- app/app.py
+++ app/app.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Add project root to Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import streamlit as st
 import pandas as pd
 
